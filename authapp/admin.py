b2eeb6949beb3f923186e8603264451f3c864fb9
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-13 13:41:20 +0530

    Initial commit

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, CertificateRequest,UserCourse
+from .models import CustomUser, CertificateRequest
 
 admin.site.register(CustomUser, admin.ModelAdmin)
 admin.site.register(CertificateRequest)
-admin.site.register(UserCourse)
--- authapp/urls.py
+++ authapp/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import GetUserById, SubmitCertificateRequest
-from .views import save_course, get_user_courses
 
 urlpatterns = [
     path('user/<int:user_id>/', GetUserById.as_view(), name='get-user'),
     path('certificate-request/', SubmitCertificateRequest.as_view(), name='certificate-request'),
-    path('save-course/', save_course, name='save-course'),
-    path('get-courses/', get_user_courses, name='get-courses'),
-
 ]
