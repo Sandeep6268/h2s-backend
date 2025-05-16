b6421d5555e1ec625918d71cbea40c60c2f0b9ed
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-16 10:07:51 +0530

    Initial commit

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetUserById, SubmitCertificateRequest,PurchaseCourseView,UserCoursesView,SubmitContactForm,CreateCashfreeOrder,cashfree_webhook
+from .views import GetUserById, SubmitCertificateRequest,PurchaseCourseView,UserCoursesView,SubmitContactForm
 
 urlpatterns = [
     path('user/<int:user_id>/', GetUserById.as_view(), name='get-user'),
@@ -7,6 +7,4 @@ urlpatterns = [
     path('purchase-course/', PurchaseCourseView.as_view(), name='purchase-course'),
     path('my-courses/', UserCoursesView.as_view(), name='user-courses'),
     path('contact/', SubmitContactForm.as_view(), name='contact-submission'),
-    path('create-cashfree-order/', CreateCashfreeOrder.as_view()),
-    path('cashfree-webhook/', cashfree_webhook),
 ]
