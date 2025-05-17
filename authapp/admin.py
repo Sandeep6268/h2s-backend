63ca0af1384ead4962bb9696e8fef3c2067fe504
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-17 23:12:36 +0530

    Initial commit

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, CertificateRequest,Course,ContactSubmission
+from .models import CustomUser, CertificateRequest,Course,ContactSubmission,PaymentRecord,UserCourseAccess
 
 admin.site.register(CustomUser, admin.ModelAdmin)
 admin.site.register(CertificateRequest)
 admin.site.register(Course)
 admin.site.register(ContactSubmission)
+admin.site.register(PaymentRecord)
+admin.site.register(UserCourseAccess)
